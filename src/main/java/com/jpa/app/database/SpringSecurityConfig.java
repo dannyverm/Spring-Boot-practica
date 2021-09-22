91ec76bcf385701a5ca49643de055364b19ffc31
Daniel veron <dannyverm@gmail.com>
2021-09-22 12:49:20 -0400

    add security from database username and password

--- src/main/java/com/jpa/app/database/SpringSecurityConfig.java
+++ src/main/java/com/jpa/app/database/SpringSecurityConfig.java
@@ -1,5 +1,7 @@
 package com.jpa.app.database;
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,17 +9,19 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.User.UserBuilder;
+
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+
 
 //esta anotacion habilita la seguridad desde los controladores
 @EnableGlobalMethodSecurity(securedEnabled = true)
 @Configuration
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    
+    @Autowired
+    private DataSource dataSource;
+
+ 
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -45,10 +49,19 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     public void configurerGlobal(AuthenticationManagerBuilder builder) throws Exception {
-        PasswordEncoder encoder = passwordEncoder();
-        UserBuilder users = User.builder().passwordEncoder(encoder::encode);
-        builder.inMemoryAuthentication()
-        .withUser(users.username("admin").password("12345").roles("ADMIN","USER"))
-        .withUser(users.username("cynthia").password("12345").roles("USER"));
+        // PasswordEncoder encoder = passwordEncoder();
+        // UserBuilder users = User.builder().passwordEncoder(encoder::encode);
+        // builder.inMemoryAuthentication()
+        // .withUser(users.username("admin").password("12345").roles("ADMIN","USER"))
+        // .withUser(users.username("cynthia").password("12345").roles("USER"));
+        builder
+        .jdbcAuthentication()
+        .dataSource(dataSource)
+        .passwordEncoder(passwordEncoder())
+        .usersByUsernameQuery("select username,password,enabled from users where username=?")
+        .authoritiesByUsernameQuery("select u.username,a.authority from authorities a inner join users u on (a.user_id=u.id) where u.username=?");
+
+
+
     }
 }
